96730678f38051edc5dd8cc1d2746b77883ba1d3
Cinta Adhiningrat <cintaamail@gmail.com>
2020-07-29 11:38:36 -0700

    Initial Commit

---
+++ Affinity_App/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Affinity_App
+//
+//  Created by Cinta Adhiningrat on 7/29/20.
+//  Copyright © 2020 Cinta Adhiningrat. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
