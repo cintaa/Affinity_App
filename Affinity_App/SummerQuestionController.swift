c40ca6325b1c7daf6d3975c7634a971862432017
Cinta Adhiningrat <cintaamail@gmail.com>
2020-07-30 11:00:52 -0700

    quiz questions cinta

---
+++ Affinity_App/SummerQuestionController.swift
@@ -0,0 +1,30 @@
+//
+//  SummerQuestionController.swift
+//  Affinity_App
+//
+//  Created by Cinta Adhiningrat on 7/30/20.
+//  Copyright © 2020 Cinta Adhiningrat. All rights reserved.
+//
+
+import UIKit
+
+class SummerQuestionController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
