db957b17a8fe0339535e02f9b0de4e570fd2e5d8
Cinta Adhiningrat <cintaamail@gmail.com>
2020-07-30 14:58:46 -0700

    quiz runthrough no final answer

---
+++ Affinity_App/WinterQuestionController.swift
@@ -0,0 +1,33 @@
+//
+//  WinterQuestionController.swift
+//  Affinity_App
+//
+//  Created by Cinta Adhiningrat on 7/30/20.
+//  Copyright © 2020 Cinta Adhiningrat. All rights reserved.
+//
+
+import UIKit
+
+class WinterQuestionController: UIViewController {
+@IBAction func nikeSweatshirtTapped(_ sender: Any) {
+    print("Success")
+    }
+@IBAction func flannelTapped(_ sender: Any) {}
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
